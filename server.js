7fefdfb89ae67337e268581f26c79ed33c8c9bbf
petenoir <tangilejeb@gmail.com>
2026-05-30 17:44:35 +0800

    Add homepage UI

--- server.js
+++ server.js
@@ -1,11 +1,10 @@
 const express = require("express");
+const path = require("path");
 
 const app = express();
 
-app.get("/", (req, res) => {
-    res.send("My Drive Server Running");
-});
+app.use(express.static(path.join(__dirname, "public")));
 
 app.listen(5000, () => {
-    console.log("Server running on port 5000");
+    console.log("Server running");
 });
