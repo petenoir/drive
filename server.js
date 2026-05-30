146d7e6f4aa120d0ba8227cfd113a5af7ef741e4
petenoir <tangilejeb@gmail.com>
2026-05-30 18:02:46 +0800

    Restore working version

--- server.js
+++ server.js
@@ -1,16 +1,11 @@
 const express = require("express");
-const path = require("path");
 
 const app = express();
 
 app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "public", "index.html"));
+    res.send("My Drive Server Running");
 });
 
-app.use(express.static(path.join(__dirname, "public")));
-
-const PORT = process.env.PORT || 5000;
-
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+app.listen(5000, () => {
+    console.log("Server running on port 5000");
 });
