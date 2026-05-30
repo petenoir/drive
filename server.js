004b2d14248db15bbd7d7b0d45ecd838465d21b1
petenoir <tangilejeb@gmail.com>
2026-05-30 18:01:05 +0800

    Fix homepage route

--- server.js
+++ server.js
@@ -3,8 +3,14 @@ const path = require("path");
 
 const app = express();
 
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
 app.use(express.static(path.join(__dirname, "public")));
 
-app.listen(5000, () => {
-    console.log("Server running");
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
 });
